2f2fea4233734ee14704536d28846cb87e06c7de
Aleksandra <marinkova_aleksandra@yahoo.com>
2026-07-22 14:39:56 +0200

    changes

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -18,11 +18,13 @@ public class EmailService : IEmailService
 
     public async Task SendBookingConfirmationAsync(string toEmail, string fullName, string court, string date, string timeSlot)
     {
-        var message = new EmailMessage();
-        message.From = "onboarding@resend.dev";
-        message.To = new List<string> { toEmail };
-        message.Subject = "Потврда за успешна резервација - Arena Booking";
-        message.HtmlBody = $"<h3>Здраво {fullName},</h3><p>Успешно го резервиравте теренот <b>{court}</b> за датум <b>{date}</b> во термин <b>{timeSlot}</b>.</p><p>Ви благодариме!</p>";
+        var message = new EmailMessage()
+        {
+            From = "onboarding@resend.dev",
+            To = toEmail,
+            Subject = "Потврда за успешна резервација - Arena Booking",
+            HtmlBody = $"<h3>Здраво {fullName},</h3><p>Успешно го резервиравте теренот <b>{court}</b> за датум <b>{date}</b> во термин <b>{timeSlot}</b>.</p><p>Ви благодариме!</p>"
+        };
 
         await _resend.EmailSendAsync(message);
     }
